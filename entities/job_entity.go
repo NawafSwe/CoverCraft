b0e5f1b0db877225c11b96f8fb0ca3061401f143
NawafSwe <nawaf.jr9@hotmail.com>
2023-09-26 10:35:03 +0300

    chore: removing json literal from db entity

--- entities/job_entity.go
+++ entities/job_entity.go
@@ -4,9 +4,9 @@ import "github.com/google/uuid"
 
 type Job struct {
 	ID             uuid.UUID `gorm:"primaryKey" json:"id"`
-	Company        string    `json:"company"`
-	Location       string    `json:"location"`
-	Description    string    `json:"description"`
-	Qualifications string    `json:"qualifications"`
-	Name           string    `json:"name"`
+	Company        string
+	Location       string
+	Description    string
+	Qualifications string
+	Name           string
 }
